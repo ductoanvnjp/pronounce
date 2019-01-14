287e5199a4b191b5fa6c2f67c99c8733294fbd46
toan_td <toan_td@septeni-technology.jp>
2019-01-14 22:42:59 +0700

    Updates

---
+++ precache-manifest.5310f502666608ee2900decafa5e4112.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8439b8a9e9bac33e8c2b",
+    "url": "/pronounce/static/js/runtime~main.8439b8a9.js"
+  },
+  {
+    "revision": "c8533f03994fdf9c22c4",
+    "url": "/pronounce/static/js/main.c8533f03.chunk.js"
+  },
+  {
+    "revision": "a75555b9cef7af214e53",
+    "url": "/pronounce/static/js/1.a75555b9.chunk.js"
+  },
+  {
+    "revision": "a75555b9cef7af214e53",
+    "url": "/pronounce/static/css/1.9d82ed0a.chunk.css"
+  },
+  {
+    "revision": "3b3b71298b655e07c80db94938079968",
+    "url": "/pronounce/index.html"
+  }
+];
